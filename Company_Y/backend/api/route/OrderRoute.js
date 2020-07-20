e8e6d1000e990b79fec0b6503964ccfe9e46fe70
diwansimran <diwansimran98@gmail.com>
2020-07-20 18:47:36 -0300

    Company Y: modified index file

--- Company_Y/backend/api/route/OrderRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const method = require('../controller/OrderController');
-
-router.get('/allorders', method.allOrders);
-router.get('/oneorder', method.specificOrder);
-router.post('/addorder', method.addOrder);
-
-module.exports = router;
